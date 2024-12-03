528ad7a889c075e2f96c79fcd49cc1d49ab5b6d7
Oxke <oxke@cyberdude.com>
2024-12-03 07:00:26 +0100

    day3

--- day1/1.jl
+++ day1/1.jl
@@ -1,7 +1,7 @@
 list1 = zeros(Int, 0)
 list2 = zeros(Int, 0)
 
-for line in eachline("input1")
+for line in eachline("input")
     n1, n2 = split(line, "   ") .|> x -> parse(Int, x)
     insert!(list1, searchsortedfirst(list1, n1), n1)
     insert!(list2, searchsortedfirst(list2, n2), n2)
