41282676d690342cb9a0c494eef33b2fb388eb07
Oxke <oxke@cyberdude.com>
2024-12-08 06:34:42 +0100

    day8

--- day8/1.jl
+++ day8/1.jl
@@ -2,20 +2,7 @@ lines = readlines("input")
 str = lines |> join
 
 grid = reduce(hcat, map(collect, lines)) |> permutedims
-# grid = reduce(hcat, map(split, lines)) |> permutedims
-
-# grid = parse.(Int, grid)
 H, W = size(grid)
-
-# lines = lines .|> x -> split(x, [':', ' '])
-# for line in lines
-#     global N
-#     res, _, parts... = line
-#     res = parse(Int, res)
-#     parts = parse.(Int, parts)
-#
-#     # DO STUFF
-# end
 
 antinodes = Set{Tuple{Int, Int}}()
 
