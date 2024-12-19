a7b481a43ce23b794e5542f11030ddcd0de63bca
Oxke <oxke@cyberdude.com>
2024-12-19 07:07:46 +0100

    day 19
    I readjusted since this day I thought it would be fun to minimize the
    number of lines lines, and I stayed in 80 chars line width.
    First solution was similar, but more spread out as they usually are. One
    more thing about today, I implemented DFS and at first had a way to
    return found in part 1 without going over all branches, but once read
    part 2 I just removed those return checks and went over all branches,
    without modifying much the solution. That's why I have only one file and
    why it was possible to solve the problem in so little lines (it still
    runs very fast btw, 300 us (see benchmark outputs))

--- day18/2.jl
+++ day18/2.jl
@@ -2,13 +2,13 @@ function parse_input(lines=nothing, str=nothing, grid=nothing, H=nothing, W=noth
     lines = readlines("input")
     str = lines |> join
 
-    walls = Set()
-    for line in lines[1:1024]
+    walls = []
+    for line in lines
         i, j = split(line, ",") |> x -> parse.(Int, x)
         push!(walls, (i, j))
     end
 
-    return walls, 70, 70, (0, 0), (70, 70), lines
+    return walls, 70, 70, (0, 0), (70, 70)
 end
 
 function cc(walls, w, H, W)
@@ -16,13 +16,14 @@ function cc(walls, w, H, W)
     vis = Set()
     stack = [w]
     while !isempty(stack)
+        print("\r", length(stack))
         i, j = pop!(stack)
         (i, j) in vis && continue
         push!(vis, (i, j))
         (j == W || i == 0) && return true
-        for di in -1:1, dj in -1:1
+        for dj in -1:1, di in 1:-1:-1
             ni, nj = i + di, j + dj
-            (ni, nj) in walls && push!(stack, (ni, nj))
+            (ni, nj) in walls && !((ni, nj) in vis) && push!(stack, (ni, nj))
         end
     end
     return false
@@ -32,15 +33,23 @@ function cc_cut(walls, H, W)
     for i in 0:H
         cc(walls, (i, 0), H, W) && return true
     end
+    println()
     return false
 end
 
-function main(walls, H, W, start, goal, lines)
-    for line in lines[1025:end]
-        i, j = split(line, ",") |> x -> parse.(Int, x)
-        push!(walls, (i, j))
-        cc_cut(walls, H, W) && return i, j
+function main(walls, H, W, start, goal)
+    lo = 1025
+    hi = length(walls)
+    while lo < hi
+        m = (lo + hi) ÷ 2
+        println(m)
+        if cc_cut(walls[1:m], H, W)
+            hi = m
+        else
+            lo = m + 1
+        end
     end
+    return walls[lo]
 end
 
 main(parse_input()...) |> println
---
+++ day19/2.jl
@@ -0,0 +1,14 @@
+function parse_input()
+    lines = readlines("input")
+    return split(lines[1], ", "), lines[3:end]
+end
+
+cache = Dict{String, Int}(""=>1)
+
+n_made(p, ts) = get!(cache, p) do
+    sum(n_made(p[length(t)+1:end], ts) for t in ts if startswith(p, t); init=0)
+end
+
+main(towels, patterns) = sum(x -> n_made(x, towels), patterns) # part 2
+
+main(parse_input()...) |> println
